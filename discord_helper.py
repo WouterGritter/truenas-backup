f11280ddd04c9630588b81c63601a7461de587ba
Wouter Gritter <woutergritter@gmail.com>
2023-10-20 17:41:51 +0200

    Update discord_helper.py

--- discord_helper.py
+++ discord_helper.py
@@ -14,19 +14,43 @@ def send_discord_message(message):
     webhook = DiscordWebhook(url=DISCORD_WEBHOOK, content=message)
     webhook.execute()
 
+def summarize_log(content, max_length):
+    lines = [l for l in content.split('\n') if should_show_log_line(l)]
+    lines_concatenated = '\n'.join(lines)
+    if len(lines_concatenated) < max_length:
+        return lines_concatenated, False
+
+    summarized = ''
+
+    # Add lines from start until a length of `max_length / 2` is reached
+    while len(lines) > 0 and len(summarized) + len(lines[0]) < max_length / 2:
+        summarized = summarized + lines[0] + '\n'
+        del lines[0]
+
+    # Add lines from end until a length of max_length is reached, if there are still lines left
+    if len(lines) > 0:
+        summarized_end = ''
+        while len(lines) > 0 and len(summarized) + len(summarized_end) + len(lines[-1]) + 6 < max_length:
+            summarized_end = lines[-1] + '\n' + summarized_end
+            del lines[-1]
+        summarized = summarized + '\n...\n\n' + summarized_end
+
+    return summarized, True
+
+def should_show_log_line(line):
+    return not ('%' in line and not '100%' in line)
+
 def start_phase():
     send_discord_message('Starting TrueNAS sync with backups now.')
 
 def end_phase(log_file):
-
     f = open(log_file, 'r')
     log_content = f.read()
     f.close()
 
-    if len(log_content) > 1000:
-        log_content = log_content[:1000] + '...'
+    log_content, is_truncated = summarize_log(log_content, 1500)
 
-    message = f'TrueNAS sync complete. Trunkated log file:\n```{log_content}```'
+    message = f'TrueNAS sync complete. {"Truncated l" if is_truncated else "L"}og file `{log_file}`:\n```{log_content}```'
     send_discord_message(message)
 
 def main():
