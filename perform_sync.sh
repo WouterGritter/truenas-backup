f38e1a4c89b9884dc1907125f221075eba59d517
Wouter Gritter <woutergritter@gmail.com>
2023-12-19 14:13:40 +0100

    Update perform_sync.sh

--- perform_sync.sh
+++ perform_sync.sh
@@ -3,12 +3,16 @@ echo "Starting sync in 60 seconds."
 echo "Starting sync in 10 seconds."
 /usr/bin/sleep 10
 
+cd ~/truenas-backup/
 /usr/bin/python3 discord_helper.py start-phase
 
 LOG_FILE=logs/rsync-$(/usr/bin/date +%Y-%m-%d--%H-%M-%S).log
 echo "Logging to file: $LOG_FILE"
 
-/usr/bin/rsync -traP --delete --backup --backup-dir=/mnt/merged/truenas-sync-backup/ root@10.43.70.121:/mnt/pool/ /mnt/merged/truenas-sync/ >> $LOG_FILE 2>&1
+/usr/bin/rsync -traP --delete --backup --backup-dir=/mnt/merged/truenas-sync-backup/ --exclude=plex/ root@10.43.70.121:/mnt/pool/ /mnt/merged/truenas-sync/ >> $LOG_FILE 2>&1
+
+echo >> $LOG_FILE
+df -h | grep 'Filesystem\|/mnt/merged' >> $LOG_FILE
 
 /usr/bin/python3 discord_helper.py end-phase $LOG_FILE
 
